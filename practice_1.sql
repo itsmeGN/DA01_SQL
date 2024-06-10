9da2e64d36a8c4be68c0733a3abb7f6c85974a04
itsmeGN <122975845+itsmeGN@users.noreply.github.com>
2024-06-10 22:38:36 +0700

    Update practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -1,1 +1,10 @@
-select *
+-- ex1: hackerank-revising-the-select-query. 
+SELECT NAME 
+FROM CITY 
+WHERE POPULATION > 120000 AND COUNTRYCODE ='USA'
+-- ex2: hackerank-japanese-cities-attributes. 
+SELECT * 
+FROM CITY
+WHERE COUNTRYCODE ='JPN'
+-- ex3: hackerank-weather-observation-station-1. 
+
