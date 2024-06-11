a0a5e143e8b5c660ae595bf7afc5f54cd4ae110c
itsmeGN <122975845+itsmeGN@users.noreply.github.com>
2024-06-11 11:33:59 +0700

    Update practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -7,4 +7,15 @@ SELECT *
 FROM CITY
 WHERE COUNTRYCODE ='JPN'
 -- ex3: hackerank-weather-observation-station-1. 
-
+SELECT CITY, STATE
+FROM STATION
+-- ex4: hackerank-weather-observation-station-6. 
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE CITY LIKE 'A%'  OR CITY LIKE 'E%' 
+OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%'
+--ex5: hackerank-weather-observation-station-7. 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%A'  OR CITY LIKE '%E'
+OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U'
